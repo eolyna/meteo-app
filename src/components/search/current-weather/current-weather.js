2d660bf8415f0ef80c2428b1a0f026103a38078c
eolyna <aubertjulie10@gmail.com>
2023-05-11 15:58:15 +0200

    adding more css

--- src/components/search/current-weather/current-weather.js
+++ src/components/search/current-weather/current-weather.js
@@ -1,18 +1,41 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
-const CurrentWeather = () =>{
-    return  (
-        <div className="weather">
-            <div className="top">
-                <div>
-                <p className="city">Troyes</p>
-                <p className="weather-description">Sunny</p>
-                </div>
-           
-            <img alt="wheather" className=" weather-icon" src="icons/01d.png" />
-        </div>
-        </div>
-    );
-}
+const CurrentWeather = () => {
+	return (
+		<div className="weather">
+			<div className="top">
+				<div>
+					<p className="city">Troyes</p>
+					<p className="weather-description">Sunny</p>
+				</div>
+				<img alt="wheather" className=" weather-icon" src="icons/01d.png" />
+			</div>
+			<div className="bottom">
+				<p className="temperature">15C</p>
+				<div className="details">
+					<div className="parameter-row">
+						<span className="parameter-label top">Details</span>
+					</div>
+					<div className="parameter-row">
+						<span className="parameter-label">Feels like</span>
+						<span className="parameter-value">13 C</span>
+					</div>
+					<div className="parameter-row">
+						<span className="parameter-label">Wind</span>
+						<span className="parameter-value">2m/s </span>
+					</div>
+					<div className="parameter-row">
+						<span className="parameter-label">Humidity</span>
+						<span className="parameter-value">15% </span>
+					</div>
+					<div className="parameter-row">
+						<span className="parameter-label">Pressure</span>
+						<span className="parameter-value">15hPa </span>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default CurrentWeather;
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,49 +3,48 @@ import { AsyncPaginate } from "react-select-async-paginate";
 import { geoApiOptions, GEO_API_URL } from "../../api";
 
 const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
+	const [search, setSearch] = useState(null);
 
-  const sleep = (ms) =>
-  new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(undefined);
-    }, ms);
-  });
+	const sleep = (ms) =>
+		new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(undefined);
+			}, ms);
+		});
 
-  async function loadOptions(search) {
+	async function loadOptions(search) {
+		await sleep(1000);
 
-    await sleep(1000);
+		const response = await fetch(
+			`${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${search}`,
+			geoApiOptions
+		);
+		const responseJSON = await response.json();
 
-    const response = await fetch(
-      `${GEO_API_URL}/cities?minPopulation=1000&namePrefix=${search}`,
-      geoApiOptions
-    );
-    const responseJSON = await response.json();
-  
-    return {
-      options: responseJSON.data.map((city) => {
-        return {
-          value: `${city.latitude} ${city.longitude}`,
-          label: `${city.name}, ${city.countryCode}`,
-        };
-      }),
-    };
-  }
+		return {
+			options: responseJSON.data.map((city) => {
+				return {
+					value: `${city.latitude} ${city.longitude}`,
+					label: `${city.name}, ${city.countryCode}`,
+				};
+			}),
+		};
+	}
 
-  const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
+	const handleOnChange = (searchData) => {
+		setSearch(searchData);
+		onSearchChange(searchData);
+	};
 
-  return (
-    <AsyncPaginate
-      placeholder="Search for city"
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
-  );
+	return (
+		<AsyncPaginate
+			placeholder="Search for city"
+			debounceTimeout={600}
+			value={search}
+			onChange={handleOnChange}
+			loadOptions={loadOptions}
+		/>
+	);
 };
 
 export default Search;
