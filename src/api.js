c2fb47f7dcfd35a0289fcbf53795a7ab71fff87c
eolyna <aubertjulie10@gmail.com>
2023-05-24 11:49:59 +0200

    fetching data from weather API

--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL ="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b0b1389d0f359224673a8a4a408e399e";
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
